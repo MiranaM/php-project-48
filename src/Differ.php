4056d49fe006a3b1d785f437f7f00fc17a0c2009
MiranaM <0resama@mail.ru>
2025-04-21 14:56:23 +0300

    link fix4,

--- src/Differ.php
+++ src/Differ.php
@@ -10,24 +10,26 @@ function genDiff(string $pathToFile1, string $pathToFile2): string
     $allKeys = array_unique(array_merge(array_keys($data1), array_keys($data2)));
     sort($allKeys);
 
-    $lines = array_map(function ($key) use ($data1, $data2) {
-        $hasKey1 = array_key_exists($key, $data1);
-        $hasKey2 = array_key_exists($key, $data2);
+    $lines = array_map(
+        function ($key) use ($data1, $data2) {
+            $hasKey1 = array_key_exists($key, $data1);
+            $hasKey2 = array_key_exists($key, $data2);
 
-        $val1 = $hasKey1 ? varToStr($data1[$key]) : null;
-        $val2 = $hasKey2 ? varToStr($data2[$key]) : null;
+            $val1 = $hasKey1 ? varToStr($data1[$key]) : null;
+            $val2 = $hasKey2 ? varToStr($data2[$key]) : null;
 
-        if ($hasKey1 && !$hasKey2) {
-            return "  - {$key}: {$val1}";
-        }
-        if (!$hasKey1 && $hasKey2) {
-            return "  + {$key}: {$val2}";
-        }
-        if ($val1 !== $val2) {
-            return "  - {$key}: {$val1}\n  + {$key}: {$val2}";
-        }
-        return "    {$key}: {$val1}";
-    }, $allKeys);
+            if ($hasKey1 && !$hasKey2) {
+                return "  - {$key}: {$val1}";
+            }
+            if (!$hasKey1 && $hasKey2) {
+                return "  + {$key}: {$val2}";
+            }
+            if ($val1 !== $val2) {
+                return "  - {$key}: {$val1}\n  + {$key}: {$val2}";
+            }
+            return "    {$key}: {$val1}";
+        }, $allKeys
+    );
 
     return "{\n" . implode("\n", $lines) . "\n}";
 }
