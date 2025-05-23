00cdd4f2823de8f4f785291a268f806421a36ea7
MiranaM <0resama@mail.ru>
2025-05-23 15:21:07 +0300

    fix 8

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -10,17 +10,24 @@ class DifferTest extends TestCase
 {
     private string $file1;
     private string $file2;
-    private string $expected;
+    private string $expectedStylish;
+    private string $expectedPlain;
 
     protected function setUp(): void
     {
         $this->file1 = __DIR__ . '/__fixtures__/file1.json';
         $this->file2 = __DIR__ . '/__fixtures__/file2.json';
-        $this->expected = file_get_contents(__DIR__ . '/__fixtures__/expected_stylish.txt');
+        $this->expectedStylish = file_get_contents(__DIR__ . '/__fixtures__/expected_stylish.txt');
+        $this->expectedPlain = file_get_contents(__DIR__ . '/__fixtures__/expected_plain.txt');
     }
 
     public function testStylishFormat(): void
     {
-        $this->assertEquals($this->expected, genDiff($this->file1, $this->file2));
+        $this->assertEquals($this->expectedStylish, genDiff($this->file1, $this->file2, 'stylish'));
+    }
+
+    public function testPlainFormat(): void
+    {
+        $this->assertEquals($this->expectedPlain, genDiff($this->file1, $this->file2, 'plain'));
     }
 }
