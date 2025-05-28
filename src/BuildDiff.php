5ee9fc28ca2d80639b5158942fda6b551fb28d1f
MiranaM <0resama@mail.ru>
2025-05-28 10:18:48 +0300

    fix9

--- src/BuildDiff.php
+++ src/BuildDiff.php
@@ -2,34 +2,34 @@
 
 namespace Differ\BuildDiff;
 
-use Illuminate\Support\Collection;
-
 function buildDiff(array $data1, array $data2): array
 {
-    return collect(array_merge(array_keys($data1), array_keys($data2)))
-        ->unique()
-        ->sort()
-        ->values()
-        ->map(function ($key) use ($data1, $data2) {
-            $oldValue = $data1[$key] ?? null;
-            $newValue = $data2[$key] ?? null;
+    $keys = array_keys(array_merge($data1, $data2));
+    $uniqueKeys = array_unique($keys);
+    sort($uniqueKeys);
 
-            return match (true) {
-                !array_key_exists($key, $data1) => ['key' => $key, 'type' => 'added', 'value' => $newValue],
-                !array_key_exists($key, $data2) => ['key' => $key, 'type' => 'removed', 'value' => $oldValue],
-                is_array($oldValue) && is_array($newValue) => [
-                    'key' => $key,
-                    'type' => 'nested',
-                    'children' => buildDiff($oldValue, $newValue),
-                ],
-                $oldValue !== $newValue => [
-                    'key' => $key,
-                    'type' => 'changed',
-                    'oldValue' => $oldValue,
-                    'newValue' => $newValue,
-                ],
-                default => ['key' => $key, 'type' => 'unchanged', 'value' => $oldValue],
-            };
-        })
-        ->all();
+    $diff = array_map(function ($key) use ($data1, $data2) {
+        $oldValue = $data1[$key] ?? null;
+        $newValue = $data2[$key] ?? null;
+
+        if (!array_key_exists($key, $data1)) {
+            return ['key' => $key, 'type' => 'added', 'value' => $newValue];
+        }
+
+        if (!array_key_exists($key, $data2)) {
+            return ['key' => $key, 'type' => 'removed', 'value' => $oldValue];
+        }
+
+        if (is_array($oldValue) && is_array($newValue)) {
+            return ['key' => $key, 'type' => 'nested', 'children' => buildDiff($oldValue, $newValue)];
+        }
+
+        if ($oldValue !== $newValue) {
+            return ['key' => $key, 'type' => 'changed', 'oldValue' => $oldValue, 'newValue' => $newValue];
+        }
+
+        return ['key' => $key, 'type' => 'unchanged', 'value' => $oldValue];
+    }, $uniqueKeys);
+
+    return $diff;
 }
